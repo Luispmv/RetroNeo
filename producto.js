9076b1e93ca6760d3996cf9fcf69b964fa86c5b3
Luispmv <luispmv07@hotmail.com>
2023-11-21 11:40:42 -0600

    actualizacion a la pagina de prducto

--- producto.js
+++ producto.js
@@ -69,3 +69,69 @@ if (backgroundProducts.length > 0) {
         camisaFour.src = "./img/ModelBackBlue.svg"
     });
 }
+
+//Carrusel
+var imagenes = [
+    "./img/CamisaDeportiva2.svg",
+    "./img/Camisa Running3.svg",
+    "./img/PantalonesRunning1.svg",
+    "./img/PantalonesRunning2.svg"
+];
+
+
+
+
+var nombresProductos = [
+    "Producto 2",
+    "Producto 3",
+    "Producto 4"
+];
+var precios = [
+    "$100",
+    "$200",
+    "$300",
+    "$400"
+];
+
+var anchos = [150];
+var altos = [150];
+
+function crearCarta(imagenURL, nombreProducto, ancho, alto, precio, i) {
+    var contenedor = document.getElementById("contenedor");
+
+    var idUnico = "carta" + (i + 1);
+
+
+    var carta = document.createElement("div");
+    carta.classList.add("card");
+
+    carta.id = idUnico;
+
+    carta.innerHTML = `
+    <img src="${imagenURL}" alt="" class="imgp">
+    <div class="Productoinfo">
+      <div class="headersProducto">
+        <p>${nombreProducto}</p>
+        <button>New</button>
+      </div>
+      <div class="headerProducto2">
+        <img src="./img/VectorHeart.svg" alt="">
+        <p>${precio}</p>
+      </div>
+    </div>
+    <button id="ver" onclick="location.reload()">Ver producto</button>
+    `;
+
+    contenedor.appendChild(carta);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    for (var i = 0; i < 4; i++) {
+        var imagenURL = imagenes[i % imagenes.length];
+        var nombreProducto = nombresProductos[i % nombresProductos.length];
+        var ancho = anchos[i % anchos.length];
+        var alto = altos[i % altos.length];
+        var precio = precios[i % precios.length];
+        crearCarta(imagenURL, nombreProducto, ancho, alto, precio, i);
+    }
+});
